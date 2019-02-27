f87a00d26e0b57c4d9719167a80a2b7016a47477
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-27 01:41:28 +0000

    Added db migration for milestones table

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,44 @@
 // Update with your config settings.
 
 module.exports = {
+	development: {
+		client: 'pg',
+		connection: {
+			database: 'test_db',
+			user: 'development',
+			password: 'development'
+		}
+	},
 
-  development: {
-    client: 'pg',
-    connection: {
-      filename: './dev.pg'
-    }
-  },
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'test_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'test_db',
-      user: 'development',
-      password: 'development'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'test_db',
-      user: 'development',
-      password: 'development'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
 };
--- migrations/20190206213957_create_milestones.js
+++ migrations/20190206213957_create_milestones.js
@@ -1,19 +1,24 @@
 
 exports.up = function (knex, Promise) {
   return Promise.all([
-    knex.schema.table('famous_people', function (table) {
+    knex.schema.createTable('milestones', function (table) {
+      table.integer('id');
       table.string('description');
       table.date('date_achieved');
+      table.integer('famous_person_id');
     })
   ])
 };
 
 exports.down = function (knex, Promise) {
   return Promise.all([
-    knex.schema.table('famous_people', function (table) {
+    knex.schema.table('milestones', function (table) {
+      table.dropColumn('id');
       table.dropColumn('description');
       table.dropColumn('date_achieved');
+      table.dropColumn('famous_person_id');
     })
   ])
 };
+
 
