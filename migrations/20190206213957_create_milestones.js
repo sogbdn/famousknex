7443c50bd5c9d64d926f28df65f1d272bff929d3
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-27 02:08:16 +0000

    New migration added

--- migrations/20190206213957_create_milestones.js
+++ migrations/20190206213957_create_milestones.js
@@ -1,24 +1,14 @@
-
-exports.up = function (knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('milestones', function (table) {
-      table.integer('id');
-      table.string('description');
-      table.date('date_achieved');
-      table.integer('famous_person_id');
-    })
-  ])
+exports.up = function(knex, Promise) {
+	return Promise.all([
+		knex.schema.createTable('milestones', function(table) {
+			table.integer('id');
+			table.string('description');
+			table.date('date_achieved');
+			table.integer('famous_person_id');
+		})
+	]);
 };
 
-exports.down = function (knex, Promise) {
-  return Promise.all([
-    knex.schema.table('milestones', function (table) {
-      table.dropColumn('id');
-      table.dropColumn('description');
-      table.dropColumn('date_achieved');
-      table.dropColumn('famous_person_id');
-    })
-  ])
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('milestones');
 };
-
-
